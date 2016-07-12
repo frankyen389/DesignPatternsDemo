7293a76a74e34916f55ecc016c42d304aebe019b
frak_yen <frank_yen@cht.com.tw>
2016-07-12 15:17:06 +0800

    刪除ObserverPattern檔案

--- src/my/observer/sample2/FirstNewsReader.java
+++
@@ -1,9 +0,0 @@
-package my.observer.sample2;
-
-import java.util.Observable;
-import java.util.Observer;
-public class FirstNewsReader implements Observer {
-    public void update(Observable obj, Object arg) {
-        System.out.println("FirstNewsReader got The news:"+(String)arg);
-    }
-}
